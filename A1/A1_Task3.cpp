d45d992161b5177846db604f6b3dd528b2ed73e9
Lovisa <lollo.99@hotmail.com>
2024-04-15 17:08:44 +0200

    task3

--- A1/A1_Task3.cpp
+++ A1/A1_Task3.cpp
@@ -7,17 +7,17 @@
 #include <cmath>
 
 // Constants
-const double c = 3*pow(10,8);
-const double dx = 0.2; // Spatial step size
-const double dt = dx/(2*c); // Time step size
-const int nx = 2/dx + 1;     // Number of space points
-const int nt = 5/dt + 1;     // Number of time steps
+const double dx = 0.001; // Spatial step size
+const double dt = 0.001; // Time step size
+const long nx = 2/dx + 1; // Number of space points
+const long nt = 5/dt + 1; // Number of time steps
 
+bool inf_space = false; // boolean for determining BCs
 
 // Set initial conditions
 void initializeFields(double Ey[], double Bz[]) {
     for (int i = 0; i < nx; ++i) {
-        // Range x from -0.1 to 0.1 
+        // Range x from -1 to 1 
         double x = -1 + i * dx;
 
         if (x > -0.1 && x<0.1) {
@@ -30,47 +30,64 @@ void initializeFields(double Ey[], double Bz[]) {
     }
 }
 
-// Function to perform FDTD update
+// Perform the FDTD of E and B
 void updateFDTD(double Ey[], double Bz[]) {
     // Update Ey
     for (int k = 1; k < nx ; ++k) {
-        // use previous value of E and B to define E at new time
-        Ey[k] = Ey[k] - dt * (Bz[k] - Bz[k-1]) / dx;
+        // Update E
+        Ey[k] = Ey[k] + dt*(Bz[k-1] - Bz[k])/dx;
     }
 
-    // Update Bz
-    for (int k = 1; k < nx ; ++k) {
-        Bz[k] = Bz[k] - dt * (Ey[k] - Ey[k-1]) / dx;
+    // Update B
+    for (int k = 0; k < nx ; ++k) {
+        Bz[k] = Bz[k] + dt*(Ey[k] - Ey[k+1]) / dx;
     }
 }
 
+// Update boundary conditions (of ideal conductor or infinite space)
+void BoundaryConditions(double Ey[], double Bz[]) {
+    // infinite space
+    if (inf_space == true){
+        Ey[0] = Ey[1];
+        Ey[nx] = Ey[nx-1];
+        Bz[0] = Bz[1];
+        Bz[nx] = Bz[nx-1];
+    }
+    // ideal condutctor
+    else{
+        Ey[0] = 0.0;
+        Ey[nx] = 0.0;
+        Bz[0] = 0.0;
+        Bz[nx] = 0.0;
+    }
+ }
+
 int main() {
-    // File for storing data
+    // Files for storing data
     std::ofstream outfile_Ey("results_Ey.txt");
     std::ofstream outfile_Bz("results_Bz.txt");
 
-    // Arrays to store electric and magnetic fields
+    // Arrays to store E and B
     double Ey[nx], Bz[nx];
-
-    // Initialize fields
+    // Initialize fields  at t = 0
     initializeFields(Ey, Bz);
 
-    // Main loop for time evolution
+    // Iterate over time
     for (int t = 0; t < nt; ++t) {
 
+        // Save E and B data
         for (int i = 0; i < nx; ++i) {
             outfile_Ey << Ey[i] << " ";
-        }
-        outfile_Ey << std::endl;
-
-        // Write current values of Bz to file
-        for (int i = 0; i < nx; ++i) {
             outfile_Bz << Bz[i] << " ";
         }
+
+        outfile_Ey << std::endl;
         outfile_Bz << std::endl;
 
-        // Update fields using FDTD method
+        // Update the fields
         updateFDTD(Ey, Bz);
+        // Update boundary conditions
+        BoundaryConditions(Ey, Bz);
     }
 
     outfile_Ey.close();
--- A1/A1_Task3_plot.py
+++ A1/A1_Task3_plot.py
@@ -1,21 +1,82 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-# Load Ey data from the file
-Ey_data = np.loadtxt('results_Bz.txt')
+def set_plot_style():
+    sns.set_context("paper", font_scale=2)
+    sns.set_style("darkgrid")
+    sns.set_palette("deep")
+    sns.set(font='sans-serif')
 
-# Create a meshgrid for time and space
+    fig_width = 10
+    fig_height = 6
+
+    plt.rcParams['figure.figsize'] = (fig_width, fig_height)
+    plt.rcParams['figure.dpi'] = 100
+    plt.rcParams['lines.linewidth'] = 1
+    plt.rcParams['lines.markersize'] = 4
+    plt.rcParams['font.size'] = 2.5 * fig_height
+    plt.rcParams['xtick.labelsize'] = 2 * fig_height
+    plt.rcParams['ytick.labelsize'] = 2 * fig_height
+    plt.rcParams['axes.labelsize'] = 2.5 * fig_height
+    plt.rcParams['axes.titlesize'] = 3 * fig_height 
+    plt.grid(True)
+
+# Set the plot style
+set_plot_style()
+
+# Load data 
+Ey_data = np.loadtxt('results_Ey.txt')
+Bz_data = np.loadtxt('results_Bz.txt')
+
+# create time and space steps
 t = np.arange(0, Ey_data.shape[0]) * 0.01  # Time
-x = np.linspace(-0.1, 0.1, Ey_data.shape[1])  # Space
+x = np.linspace(-1, 1, Ey_data.shape[1])  # Space
 
-# Create meshgrid for plotting
+# create meshgrid for t and x
 T, X = np.meshgrid(t, x)
 
-# Plot meshgrid
-plt.figure(figsize=(10, 6))
-plt.pcolormesh(T, X, Ey_data.T, cmap='viridis')  # Transpose Ey_data
-plt.colorbar(label='Ey')
-plt.xlabel('Time')
-plt.ylabel('Space')
-plt.title('Meshgrid for Ey over Time and Space')
+""" # Plot Ey
+plt.pcolormesh(X, T, Ey_data.T, cmap='viridis', shading='auto')
+plt.colorbar(label='E-field ($\hat{y}$)')
+plt.xlabel('Space ($\hat{x}$)')
+plt.ylabel('Time')
+plt.title('Change in E-field over Time and Space - Inifinite Space')
+plt.tight_layout()
+plt.xlim(-0.1, 0.2)
+plt.ylim(0,0.5)
+plt.savefig('Figures/A1_E_inf_zoom.png')
+#plt.savefig('Figures/A1_E_inf.png')
 plt.show()
+
+# Plot Bz
+plt.pcolormesh(X, T, Bz_data.T, cmap='viridis', shading='auto')
+plt.colorbar(label='B-field ($\hat{z}$)')
+plt.xlabel('Space ($\hat{x}$)')
+plt.ylabel('Time')
+plt.title('Change in B-field over Time and Space - Inifinite Space')
+plt.tight_layout()
+plt.savefig('Figures/A1_B_inf.png')
+plt.show()
+ """
+plt.pcolormesh(X, T, Ey_data.T, cmap='plasma', shading='auto')
+plt.colorbar(label='E-field ($\hat{y}$)')
+plt.xlabel('Space ($\hat{x}$)')
+plt.ylabel('Time')
+plt.title('Change in E-field over Time and Space - Ideal Conductor')
+plt.tight_layout()
+""" plt.xlim(-0.1, 0.2)
+plt.ylim(0,0.5) """
+#plt.savefig('Figures/A1_E_IC_zoom.png')
+plt.savefig('Figures/A1_E_IC.png')
+plt.show()
+
+# Plot Bz
+plt.pcolormesh(X, T, Bz_data.T, cmap='viridis', shading='auto')
+plt.colorbar(label='B-field ($\hat{z}$)')
+plt.xlabel('Space ($\hat{x}$)')
+plt.ylabel('Time')
+plt.title('Change in B-field over Time and Space - Ideal Conductor')
+plt.tight_layout()
+plt.savefig('Figures/A1_B_IC.png')
+plt.show()  
